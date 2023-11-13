5d06cfe24db17b870e3d2e8d5d798b66e457d4de
Danilo Augusto <127322985+dandanilo2010@users.noreply.github.com>
2023-11-13 18:00:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function cliqueiNoBotao(){
+   formulario.style.left = "600px"
+}
+
+let formulario = document.querySelector('.fale-conosco')
+
+function sumirFormulario(){
+    formulario.style.left = "-320px"
+}
+
